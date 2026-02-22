fc8678cef46e71bcc853c57ab7b52ff1ee7d7d0b
Maksim Shirokov <nimbodex@yandex.ru>
2026-02-23 00:38:33 +0500

    Improve decoding functionality, add extra tests

--- src/vlc/decode.rs
+++ src/vlc/decode.rs
@@ -1,14 +1,12 @@
-use crate::vlc::hex::{HexChunk, HexChunks, DEFAULT_SEPARATOR};
+use crate::vlc::hex::{HexChunk, HexChunks};
 
-pub fn decode(str: String) -> HexChunks {
-    let parts: Vec<&str> = str.split(DEFAULT_SEPARATOR).collect();
-    let mut result = Vec::with_capacity(parts.len());
-    
-    for part in parts {
-        result.push(HexChunk(part.to_string()));
-    }
-    
-    HexChunks(result)
+pub fn decode(input: &str) -> HexChunks {
+    HexChunks::new(
+        input
+            .split_whitespace()
+            .map(|p| HexChunk::new(p.to_string()))
+            .collect(),
+    )
 }
 
 #[cfg(test)]
@@ -18,15 +16,35 @@ mod tests {
 
     #[test]
     fn decode_tests() {
-        let cases = vec![(
-            "20 30 3C 18".to_string(),
-            HexChunks::new(vec![
-                HexChunk::new("20".to_string()),
-                HexChunk::new("30".to_string()),
-                HexChunk::new("3C".to_string()),
-                HexChunk::new("18".to_string()),
-            ]),
-        )];
+        let cases = vec![
+            (
+                "20 30 3C 18",
+                HexChunks::new(vec![
+                    HexChunk::new("20".to_string()),
+                    HexChunk::new("30".to_string()),
+                    HexChunk::new("3C".to_string()),
+                    HexChunk::new("18".to_string()),
+                ]),
+            ),
+            (
+                "",
+                HexChunks::new(vec![]),
+            ),
+            (
+                "20 30",
+                HexChunks::new(vec![
+                    HexChunk::new("20".to_string()),
+                    HexChunk::new("30".to_string()),
+                ]),
+            ),
+            (
+                " 20 30 ",
+                HexChunks::new(vec![
+                    HexChunk::new("20".to_string()),
+                    HexChunk::new("30".to_string()),
+                ]),
+            )
+        ];
 
         for (str, expected) in cases {
             assert_eq!(decode(str), expected, "base")
